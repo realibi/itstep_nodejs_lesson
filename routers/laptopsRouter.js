ae680511d90b5235eec4a0a96c189ec018250212
Асымов Омирхан Ильясович <@lcoast.itstep>
2022-05-24 17:23:47 +0600

    laptops id

--- routers/laptopsRouter.js
+++ routers/laptopsRouter.js
@@ -6,25 +6,42 @@ let laptopsArray = [
     {
         manufacturer: "Acer",
         name: "Nitro 5",
-        price: 392800
+        price: 392800,
+        id: 1
     },
 
     {
         manufacturer: "Lenovo",
         name: "IdeaPad Gaming 3",
-        price: 428755
+        price: 428755,
+        id: 2
     },
 
     {
         manufacturer: "HP",
         name: "Victus 16",
-        price: 439900
+        price: 439900,
+        id: 3
     }
 ];
 
 laptopsRouter.get("/", (request, response) => {
     response.render("laptops.hbs", {
-        laptopsarray: laptopsArray
+        laptopsarray: laptopsArray,
+        showArray: true
+    });
+});
+
+laptopsRouter.get("/:id", (request, response) => {
+    let idx = request.params.id;
+
+    let foundIndex = laptopsArray.findIndex(item => item.id == idx);
+
+    response.render("laptops.hbs", {
+        manufacturer: laptopsArray[foundIndex].manufacturer,
+        name: laptopsArray[foundIndex].name,
+        price: laptopsArray[foundIndex].price,
+        showArray: false
     });
 });
 
