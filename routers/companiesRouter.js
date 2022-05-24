2e95273e97e1b7157df3d7e4c2a399d072c50ce1
Маханбетова Аружан Нурбеккызы <@lcoast.itstep>
2022-05-24 17:15:28 +0600

    company id added

--- routers/companiesRouter.js
+++ routers/companiesRouter.js
@@ -1,17 +1,28 @@
 let express = require("express");
 let companiesRouter = express.Router();
 
+let companiesArray = [
+    {id: 1, name: 'Air Astana', year: 2001},
+    {id: 2, name: 'Berkut Air', year: 2000},
+    {id: 3, name: 'Euro-Asia Air', year: 1997},
+    {id: 4, name: 'SCAT', year: 1997},
+    {id: 5, name: 'Semeyavia', year: 1991}
+]
+
 companiesRouter.get('/', (request, response) => {
     response.render('companies.hbs', {
-        company: [
-            {name: 'Air Astana', year: 2001},
-            {name: 'Berkut Air', year: 2000},
-            {name: 'Euro-Asia Air', year: 1997},
-            {name: 'SCAT', year: 1997},
-            {name: 'Semeyavia', year: 1991}
-        ]
+        company: companiesArray
     })
     // response.json(companiesArray);
+});
+
+
+companiesRouter.get('/:id', (request, response) => {
+    let companyId = request.params.id;
+    let idx = companiesArray.findIndex(item => item.id == companyId)
+    response.render('companies.hbs', {
+        findCompanyId: companiesArray[idx]
+    })
 });
 
 companiesRouter.post('/', (req, res) => {
