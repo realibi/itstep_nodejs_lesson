354c7e042c68c3e8de3c0c71e4d8c87d5e03ddf1
realibi <reallibi@gmail.com>
2022-05-24 16:35:08 +0600

    hbs added

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const usersRouter = require("./routers/usersRouter")
 const coffeesRouter = require("./routers/coffeesRouter")
 app.use(cors());
 
+app.set('view engine', 'hbs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -19,5 +21,19 @@ app.use("/laptops", laptopsRouter);
 app.use("/companies", companiesRouter);
 app.use("/users", usersRouter);
 app.use("/coffees", coffeesRouter);
+
+app.get('/contacts', (req, res) => {
+    res.render('contacts.hbs', {
+        email: "privet",
+        phone: "877777777",
+        userAddress: "abaya 24",
+        showPhone: true,
+        branches: [
+            {address: "abaya 24", phone: "475623"},
+            {address: "imanova 7", phone: "669944"},
+            {address: "seifullina 40", phone: "345678"}
+        ]
+    });
+});
 
 app.listen(8080);
